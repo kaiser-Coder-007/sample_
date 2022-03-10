9e1fd8111aed1080a653e30a3ed4a82e0a889f29
kaiser-Coder-007 <merajunnesha123@gmail.com>
2022-03-10 22:43:38 +0530

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+console.log("javascript is on.....")
+function True(){
+    return true
+    console.log('true')
+}
+let contact = document.getElementById('contact');
+let back = document.getElementById('back');
+
+function opacity_init(){
+    back.style.opacity = 0.6;
+}
+
+function opacity_exit(){
+    back.style.opacity = 0.3;
+}
